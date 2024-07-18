db20a83588c23899903d6fe9ec2ddf195c41a4bc
JahiemM <mcleodjaheim@Gmail.com>
2024-07-18 06:36:34 -0400

    Update lab 4 and Children app

--- children_app/script.js
+++ children_app/script.js
@@ -1,50 +1,129 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+// Use texttospeech to vocalize the words of the animal and 
+const synth = window.speechSynthesis;
+
 let menuBtn = document.querySelector(".nav-menu");
 let menu = document.querySelector(".menu");
 let back = document.querySelector(".nav-back");
 let navLinks = document.querySelector(".nav-link");
 let animals = document.querySelectorAll(".animal");
 let modal = document.querySelector(".modalWindow");
+let closeModal = document.querySelector(".closeModal")
+let speech = document.querySelector(".en-tts");
+let spanish = document.querySelector(".esp-tts")
+let animalSound = document.querySelector(".roar");
+let voices = [];
 
 let animalSounds = [
     {
         Name: "Bear",
-        sound: "./Sounds/Bear/roar.mp3",
+        sound: new Audio("./Sounds/Bear/roar.mp3"),
+        info: ""
+    }, 
+    {
+        name: "bull",
+        sound: new Audio("./Sounds/Bear/roar.mp3"),
         info: ""
     }
 ];
 
-menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("show");
-    // trigger animation slide when opening menu
-});
+let toggle = (index, apply) => {
+    index.classList.toggle(apply);
+}
 
-back.addEventListener("click", () => {
-    menu.classList.toggle("show");
-});
+let toggleMenu = () => {
+    toggle(menu, "show");
+}
 
-navLinks.addEventListener("click", () => {
-    menu.classList.toggle("show");
-});
+menuBtn.addEventListener("click", toggleMenu)
+back.addEventListener("click", toggleMenu)
+navLinks.addEventListener("click", toggleMenu)
 
 /* Animals and languages */
 animals.forEach(animal => {
     animal.addEventListener("mousedown", () => {
         animal.classList.toggle('selected');
+        console.log(animal);
     });
 
     animal.addEventListener("mouseup", () => {
         animal.classList.toggle('selected');
-        modal.classList.toggle('active');
+        modal.style.display = "block";
         console.log("mouseup");
-    })
+    });
 
+    animalSound.addEventListener("click", () => {
+        let name = animal.dataset.type;
+        console.log(name);
+        
+       for (let i = 0; i < animalSounds.length; i++) {
+        let audioOn = false;
+            console.log("True");
+            animalSounds[i].sound.play();
+            audioOn = true;
+        }
+
+    });
 });
 
 
 
+
+const loadVoices = () => {
+    voices = synth.getVoices();
+        if (voices.length > 0) {
+            console.log('Voices list:', voices);
+            addSpeechEventListeners();
+        }
+
+};
+    const addSpeechEventListeners = () => {
+        speech.addEventListener('click', () => {
+           const utterance = new SpeechSynthesisUtterance("Bear");
+           /* utterance.voice = voice.find(voice => voice.name === "Google US English"); */
+           utterance.pitch = 1;
+           utterance.rate = 1;
+           utterance.volume = 1;
+          /*  utterance.voice = "Google US English"; */
+           synth.speak(utterance);
+           console.log("clicked");
+        });
+
+        spanish.addEventListener('click', () => {
+            const utterance = new SpeechSynthesisUtterance("Oso");
+           /* utterance.voice = voice.find(voice => voice.name === "Google US English"); */
+           utterance.pitch = 1;
+           utterance.rate = 1;
+           utterance.volume = 1;
+           utterance.voice = voices.find(voice => voice.lang == 'es-ES');
+          /*  utterance.voice = "Google US English"; */
+           synth.speak(utterance);
+           console.log("clicked");
+        })
+    };
+    
+
+
+    loadVoices();
+    if (typeof window.speechSynthesis.onvoiceschanged !== 'undefined') {
+        window.speechSynthesis.onvoiceschanged = loadVoices;
+    } else {
+    console.log('Text-to-Speech is not supported in this browser.');
+    };
+
+        
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+/* user clicks outside of modal close the modal */
+window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 });
 
 
-
-
+});
