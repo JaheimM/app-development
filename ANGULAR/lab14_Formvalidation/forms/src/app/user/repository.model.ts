9784bdf99a5dfba89007d39dc4d4c6673a1aaf7d
JahiemM <mcleodjaheim@Gmail.com>
2024-08-06 20:51:15 -0400

    lab 14 - form validation

---
+++ ANGULAR/lab14_Formvalidation/forms/src/app/user/repository.model.ts
@@ -0,0 +1,32 @@
+// repository.model.ts file us used to set up all the elements needed to set up the form
+
+import { User } from "./user.model";
+import { Datasource } from "./datasource.model"
+
+export class User_repository {
+    private datasource:Datasource
+    private users:User[]
+
+    constructor() {
+        this.datasource = new Datasource()
+        this.users = new Array<User>()
+        this.datasource.getuser().forEach(u => {
+            this.users.push(u)
+        });
+    }
+
+        getuser():User[] {
+            return this.users
+        }
+
+        getuserID(id:number):any {
+            return this.users.find(u => {
+                u.id == id
+            })
+        }
+        
+        adduser(user:User) {
+            this.users.push(user)
+        }
+
+}
